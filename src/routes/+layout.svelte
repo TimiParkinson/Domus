fa79d9b9c1cad894b0ce59608e9510f4c16a5f1c
TimiParkinson <119539277+TimiParkinson@users.noreply.github.com>
2024-11-22 16:15:59 +0000

    Initial Page Structure

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">Domus</a>
+	<a href="/about">About</a>
+    <a href="/contact">Contact</a>
+	<a href="/sales">Sales</a>
+</nav>
+
+{@render children()}
